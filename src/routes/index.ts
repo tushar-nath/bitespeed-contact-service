ef06c2813c44a2084ade27bb7868b22d48204c0d
tushar-nath <tusharnath10@gmail.com>
2024-05-24 23:46:41 +0530

    CHORE: Added healthcheck route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { identify } from "../handler/contactHandler";
 
 const router = Router();
 
+router.get("/healthcheck", (_req: Request, res: Response) => {
+  res.send({ success: true });
+});
 router.post("/identify", identify);
 
 export default router;
