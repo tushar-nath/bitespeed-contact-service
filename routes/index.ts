9ba332016baf2b696adce806176b367d991210f2
tushar-nath <tusharnath10@gmail.com>
2024-06-09 16:18:01 +0530

    CHORE: Update homepage message

--- routes/index.ts
+++ routes/index.ts
@@ -4,7 +4,7 @@ import { identify } from '../handlers/contactHandler'
 export const v1Router = Router()
 
 v1Router.get('/', (_req: Request, res: Response) => {
-  return res.send('Express Typescript on Vercel')
+  return res.send('Welcome to Bitespeed Contact Service!')
 })
 v1Router.get('/healthcheck', (_req: Request, res: Response) => {
   res.send({ success: true })
